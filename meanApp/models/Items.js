ed123bdf579cdaee7653436d9af9459506b71a97
Jonathan Wood <bamblehorse@gmail.com>
2016-11-28 12:13:06 +0000

    added upvote method to items schema

--- meanApp/models/Items.js
+++ meanApp/models/Items.js
@@ -7,4 +7,9 @@ var ItemSchema = new mongoose.Schema({
 	comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
 });
 
+ItemSchema.methods.upvote = function(cb) {
+	this.upvotes += 1;
+	this.save(cb);
+};
+
 mongoose.model('Item', ItemSchema);
